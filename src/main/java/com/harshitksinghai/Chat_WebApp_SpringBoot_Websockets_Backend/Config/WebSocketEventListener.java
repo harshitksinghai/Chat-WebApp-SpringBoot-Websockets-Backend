c440bfbd60c93f40e62053edb53c42e97abddc98
HarshitSinghai <harshitksinghai.official@gmail.com>
2024-08-26 15:50:04 +0530

    add WebSocketEventListener base code

---
+++ src/main/java/com/harshitksinghai/Chat_WebApp_SpringBoot_Websockets_Backend/Config/WebSocketEventListener.java
@@ -0,0 +1,17 @@
+package com.harshitksinghai.Chat_WebApp_SpringBoot_Websockets_Backend.Config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WebSocketEventListener {
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event){
+
+    }
+}
