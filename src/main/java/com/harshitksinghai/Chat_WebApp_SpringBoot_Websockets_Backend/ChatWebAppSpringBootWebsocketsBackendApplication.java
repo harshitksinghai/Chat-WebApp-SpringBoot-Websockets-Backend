7dedfb1d7cf0fcc2a5757db447869ec577d93d71
HarshitSinghai <harshitksinghai.official@gmail.com>
2024-08-26 12:46:54 +0530

    first commit

---
+++ src/main/java/com/harshitksinghai/Chat_WebApp_SpringBoot_Websockets_Backend/ChatWebAppSpringBootWebsocketsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.harshitksinghai.Chat_WebApp_SpringBoot_Websockets_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatWebAppSpringBootWebsocketsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatWebAppSpringBootWebsocketsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harshitksinghai/Chat_WebApp_SpringBoot_Websockets_Backend/ChatWebAppSpringBootWebsocketsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harshitksinghai.Chat_WebApp_SpringBoot_Websockets_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatWebAppSpringBootWebsocketsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
